84d1e4031a8236316ce78951e567712cb93e120f
nmwh <43128952+do0401@users.noreply.github.com>
2019-07-23 11:06:24 +0900

    26일차 commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
+import {
+  RSA_NO_PADDING
+} from "constants";
 
 export const getJoin = (req, res) => {
   res.render("join", {
@@ -154,10 +157,33 @@ export const userDetail = async (req, res) => {
   }
 };
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
   res.render("editProfile", {
     pageTitle: "Edit Profile"
   });
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: {
+      name,
+      email
+    },
+    file
+  } = req;
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl
+    });
+    res.redirect(routes.me);
+  } catch (error) {
+    res.render("editProfile", {
+      pageTitle: "Edit Profile"
+    });
+  }
+};
+
 export const changePassword = (req, res) =>
   res.render("changePassword", {
     pageTitle: "Change Password"
--- middlewares.js
+++ middlewares.js
@@ -4,6 +4,9 @@ import routes from "./routes";
 const multerVideo = multer({
   dest: "uploads/videos/"
 });
+const multerAvatar = multer({
+  dest: "uploads/avatars/"
+});
 
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
@@ -29,3 +32,4 @@ export const onlyPrivate = (req, res, next) => {
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
