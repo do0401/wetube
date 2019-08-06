79a5da055490ed5671f2c1e11b88613c3d330229
nmwh <43128952+do0401@users.noreply.github.com>
2019-08-06 11:00:58 +0900

    28일차 마지막 commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   "extends": ["airbnb-base", "plugin:prettier/recommended"],
   "rules": {
-    "linebreak-style": [2, "windows"],   // windows OS configuration
+    "linebreak-style": [2, "windows"], // windows OS configuration
     "no-console": "off"
+  },
+  env: {
+    "browser": true
   }
 };
