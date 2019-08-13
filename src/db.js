150cd0ddee26f0f9dfcd3969f05f5a50f571f8b6
nmwh <43128952+do0401@users.noreply.github.com>
2019-08-13 17:34:52 +0900

    Edit db.js

--- src/db.js
+++ src/db.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 console.log(process.env.PRODUCTION);
 mongoose.connect(
-  process.env.PRODUCTION === "true" ? process.env.MONGO_PROD : process.env.MONGO_URL, {
+  // process.env.PRODUCTION === "true" ? process.env.MONGO_PROD : process.env.MONGO_URL, {
+  process.env.MONGO_PROD, {
     useNewUrlParser: true,
     useFindAndModify: false
   }
